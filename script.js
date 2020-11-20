f874f002c6528c57ed7001b64210f9424834f711
JonGudenzi <jongudenzi@gmail.com>
2020-11-20 16:26:12 -0500

    testing

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 $(function(){
 
+
 var today = moment();
 $("#currentDay").text(today.format("dddd MMMM Do YYYY, h:mm a"));
 
-var hour = today.hours();
+
+var currentHour = parseInt(moment().format("H"));
 
 for (i = 0; i < 8; i++){
    var plannerRow = $("<div>").addClass("row")
    var time = $("<div>").text((i+9)+":00").addClass("col-1")
-   var text = $("<textarea>").addClass("col-10")
+   var text = $("<textarea>").addClass("col-10", "input")
    var save = $("<button>").text("save").addClass("col-1 btn btn-success")
    plannerRow.append(time, text, save)
    $(".container").append(plannerRow)
@@ -22,13 +24,10 @@ localStorage.setItem("text", textInput);
 }
 
 
-
-
-
-
-if(today < hour){
-   text.addClass("past")
+   if (currentHour > time){
+      $("text").addClass("past");
    }
+
 
 
 
