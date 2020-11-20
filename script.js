a5919e67435cbd327680a8a3edbdc54c780dab85
JonGudenzi <jongudenzi@gmail.com>
2020-11-20 12:55:27 -0500

    added click event button and local storage saving for text area

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+$(function(){
+
 var today = moment();
 $("#currentDay").text(today.format("dddd MMMM Do YYYY, h:mm a"));
+
+var hour = today.hours();
+
+for (i = 0; i < 8; i++){
+   var plannerRow = $("<div>").addClass("row")
+   var time = $("<div>").text((i+9)+":00").addClass("col-1")
+   var text = $("<textarea>").addClass("col-10")
+   var save = $("<button>").text("save").addClass("col-1 btn btn-success")
+   plannerRow.append(time, text, save)
+   $(".container").append(plannerRow)
+}
+
+$("button").on("click", saveNote)
+function saveNote(){
+   // console.log($(this).siblings("textarea").val())
+   var textInput = $(this).siblings("textarea").val()
+localStorage.setItem("text", textInput);
+}
+
+
+
+
+
+
+if(today < hour){
+   text.addClass("past")
+   }
+
+
+
+// <=> the hour loop   if / else if  / else  (past,presnt, or future)
+})
 
