5c3c6093c40ef00c8349f8108c5ca14b54102bef
JonGudenzi <jongudenzi@gmail.com>
2020-11-20 20:08:16 -0500

    trying to get local storage to save for every row.  Trying to get if else statements to work

--- script.js
+++ script.js
@@ -1,36 +1,60 @@
 $(function(){
-
-
+debugger;
 var today = moment();
-$("#currentDay").text(today.format("dddd MMMM Do YYYY, h:mm a"));
-
+$("#currentDay").text(today.format("dddd MMMM Do YYYY, h:mm A"));
 
 var currentHour = parseInt(moment().format("H"));
 
+// debugger;
 for (i = 0; i < 8; i++){
-   var plannerRow = $("<div>").addClass("row")
-   var time = $("<div>").text((i+9)+":00").addClass("col-1")
-   var text = $("<textarea>").addClass("col-10", "input")
-   var save = $("<button>").text("save").addClass("col-1 btn btn-success")
-   plannerRow.append(time, text, save)
-   $(".container").append(plannerRow)
+   var plannerRow = $("<div>").addClass("row");
+   var time = $("<div>").text((i+9)+":00").addClass("col-1").attr("id", "times");
+
+   var text = $("<textarea>").each(function(){
+      $(this).attr("id", "id_" + i);
+   }).addClass("col-10");
+
+   var save = $("<button>").text("save").addClass("col-1 btn btn-success");
+   plannerRow.append(time, text, save);
+   $(".container").append(plannerRow);
 }
 
-$("button").on("click", saveNote)
-function saveNote(){
-   // console.log($(this).siblings("textarea").val())
-   var textInput = $(this).siblings("textarea").val()
-localStorage.setItem("text", textInput);
+times = time[0].innerText;
+console.log(times);
+
+text.each(function(){
+if (currentHour < times){
+   $(this).addClass("past");
 }
 
+else if (currentHour > times){
+   $(this).addClass("future");
+}
 
-   if (currentHour > time){
-      $("text").addClass("past");
-   }
-
-
-
-
-// <=> the hour loop   if / else if  / else  (past,presnt, or future)
+else (currentHour == times)
+   $(this).addClass("present");
 })
 
+// console.log("time: ", time[0].innerText);
+
+$("button").on("click", saveNote);
+function saveNote(){
+   var textInput = $(this).siblings("textarea").val();
+   var textHistory = JSON.parse(localStorage.getItem("text")) || [];
+   textHistory.push(textInput);
+localStorage.setItem("text", JSON.stringify(textHistory));
+}
+
+for (let i = 0; i < localStorage.length; i++){
+   var retrieveData = localStorage.getItem("text");
+   var schedules = JSON.parse(retrieveData);
+   //alert(schedules.length);
+
+   for (let i=0; i < schedules.length; i++);
+   textArea.val = schedules.join("");
+}
+
+
+
+})
+ 
